d6141d38acbb409c2d607ab232b36e80889255b5
Malli-M <100527968+Malli-M@users.noreply.github.com>
2024-12-01 19:04:48 +0530

    Update etl_job.py

--- etl_job.py
+++ etl_job.py
@@ -2,17 +2,21 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
 def transform_data(input_path, output_path):
-  spark = SparkSession.builder.appName("ETL job").getOrCreate()
-  #Extract: read csv data
-
-  df = spark.read.option("header","true").csv(input_path)
-  transformred_df = (df.filter(col("amount")>100)
-                    .withColumn("double_amount", col("amount")*2)
-                 )
-  #load: write the transfomred data to CSV
-  transformred_df.write.mode("overwrite").csv(output_path, header=True)
-  spark.stop()
-
+    spark = SparkSession.builder.appName("ETL job").getOrCreate()
+    
+    # Extract: read CSV data
+    df = spark.read.option("header", "true").csv(input_path)
+    
+    # Transformation: Filter and add 'double_amount' column
+    transformed_df = (df.filter(col("amount") > 100)
+                       .withColumn("double_amount", col("amount") * 2)
+    )
+    
+    # Load: Write the transformed data to CSV
+    transformed_df.write.mode("overwrite").csv(output_path, header=True)
+    
+    # Stop Spark session after all operations
+    spark.stop()
 
 if __name__ == "__main__":
     input_path = "data/input.csv"
