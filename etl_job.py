9506b8fe7b429256b23fd1921e14d4609413e134
Malli-M <100527968+Malli-M@users.noreply.github.com>
2024-12-01 17:46:02 +0530

    Create etl_job.py
    Using pyspark applying the filter and write to csv file

---
+++ etl_job.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+def transform_data(input_path, output_path):
+  spark = SparkSession.builder.appName("ETL job").getOrCreate()
+  #Extract: read csv data
+
+  df = spark.read.option("header","true").csv(input_path)
+  transformred_df = (df.filter(col("amount")>100)
+                    .withColumn("double_amount", col("amount")*2)
+                 )
+  #load: write the transfomred data to CSV
+  transfomred_df.write.mode("overwrite").csv(ouput_path, header=True)
+  spark.stop()
+
+
+if__name__ == "__main__":
+    input_path = "data/input.csv"
+    output_path = "data/output"
+    transform_data(input_path, output_path)
